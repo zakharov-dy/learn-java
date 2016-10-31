b384ddaf94d89c063d5d7b2a749eb7899cad8d6b
matrix.predictor <matrix.predictor@gmail.com>
2016-10-31 08:27:04 +0500

    upd Pile

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -9,16 +9,28 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         Item item1 = new Item( 12, "item", new HashSet<String>(Arrays.asList("Ха", "хА")));
         Item item2 = new Item( 14, "item2", new HashSet<String>(Arrays.asList("Ха", "хА", "ХАхаХа")));
-        System.out.println(item1.getInfo());
-        Bag bag = new Bag(1, 15, "Bag", new HashSet<String>(Arrays.asList("Ха", "хА")));
-        bag.addItem(item1);
-        System.out.println(bag.getWeight());
-        bag.addItem(item2);
-        Item removedItem = bag.removeRandomItem();
-        System.out.println(removedItem.getInfo());
-        bag.addItem(item2);
-        System.out.println(bag.getWeight());
-        System.out.println(bag.searchItem("item2").getInfo());
-        bag.searchItem("item1");
+        Item item3 = new Item( 14, "item3", new HashSet<String>(Arrays.asList("flat")));
+        Item item4 = new Item( 14, "item4", new HashSet<String>(Arrays.asList("flat")));
+        Item item5 = new Item( 14, "item5", new HashSet<String>(Arrays.asList("flat")));
+//        System.out.println(item1.getInfo());
+//        Bag bag = new Bag(1, 15, "Bag", new HashSet<String>(Arrays.asList("Ха", "хА")));
+//        bag.addItem(item1);
+//        System.out.println(bag.getWeight());
+//        bag.addItem(item2);
+//        Item removedItem = bag.removeRandomItem();
+//        System.out.println(removedItem.getInfo());
+//        bag.addItem(item2);
+//        System.out.println(bag.getWeight());
+//        System.out.println(bag.searchItem("item2").getInfo());
+//        bag.searchItem("item1");
+        Pile pile = new Pile(2, "pile");
+//        pile.addItem(item3);
+//        pile.addItem(item4);
+//        pile.addItem(item5);
+//        System.out.println(pile.isFlat(item5));
+        System.out.println(pile.addItem(item3));
+        System.out.println(pile.addItem(item4));
+        System.out.println(pile.addItem(item5));
+        System.out.println(pile.items.size());
     }
 }
--- JavaApplication1/src/javaapplication1/Pile.java
+++ JavaApplication1/src/javaapplication1/Pile.java
@@ -7,19 +7,13 @@ import java.util.PriorityQueue;
 class Pile extends ItemsContainer {
     private final float maxSize;
     PriorityQueue items = new PriorityQueue();
-    public Pile(float w, float ms, String n, HashSet<String> p) {
-        super(w, n, p);
+    public Pile(int ms, String n) {
+        super(0, n, null);
         maxSize = Math.abs(ms);
     }
     @Override
-    public boolean addItem(Item i) { return ( !isFull() && isFlat(i)) ? false : super.addItem(i); }
+    public boolean addItem(Item i) { return ( !isFull() && isFlat(i)) ? super.addItem(i) : false; }
     public boolean isFull() { return items.size() == maxSize; }
     public boolean isFlat(Item i) { return i.getProps().contains("flat"); } 
-    public Item pollItem() {
-        if (items.isEmpty()) {
-            System.out.println("empty");
-        }
-        
-    }
-
+    public Item pollItem() { return (Item) items.poll(); }
 }
