9ab84a1eb0fb16b91a1adf402310430a5abebe2e
Asus <Asus@DESKTOP-VDJPA9K>
2016-12-27 17:30:06 +0500

    сдал JDBC

--- DBTester/src/dbtester/DBTester.java
+++ DBTester/src/dbtester/DBTester.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -22,20 +23,26 @@ public class DBTester {
   }
   public static void main(String[] args) throws SQLException {
     DBTester dbt = new DBTester();
-//    Connection connect = dbt.connectToDB();
     dbt.doWork();
   }
   void doWork() throws SQLException {
-//    createTablesIfNeeded();
-//    viewGroups();
-//    viewItems();
-//    System.out.println(getTitleID("ITEMGROUP", "КОМПЬЮТЕРЫ"));
-//    viewItemsInGroup(3);
-//    System.out.println(addItemToGroup("Mac", "КОМПЬЮТЕРЫ"));
-//    viewItems();
-//    createItemsAcrossFile("src/files/items.txt");
-    updateGroupsAcrossFile("src/files/groups.txt");
-  }
+    try {
+      createTablesIfNeeded();
+      viewGroups();
+      viewItems();
+      System.out.println(getTitleID("ITEMGROUP", "КОМПЬЮТЕРЫ"));
+      viewItemsInGroup(3);
+      System.out.println(addItemToGroup("Mac", "КОМПЬЮТЕРЫ"));
+      viewItems();
+      createItemsAcrossFile("src/files/items.txt");
+      updateGroupsAcrossFile2("src/files/groups.txt");
+    } catch (SQLException ex) {
+      Logger.getLogger(DBTester.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    finally {
+      conn.close();
+    }
+  } 
   private Connection connectToDB() throws SQLException {
     String driverName = "org.apache.derby.jdbc.ClientDriver40";
     try {
@@ -44,37 +51,38 @@ public class DBTester {
       logger.log(Level.SEVERE, "ERROR: Driver <{0}> not found", driverName);
       System.exit(0);
     }
-    
     String dbUrl = "jdbc:derby://localhost:1527/groupdb";
-    
     Properties connInfo = new Properties();
     connInfo.put("user", "java");
     connInfo.put("password", "java");
-    
-    Connection conn = null;
-    conn = DriverManager.getConnection(dbUrl, connInfo);
+    Connection conn = conn = DriverManager.getConnection(dbUrl, connInfo);
     return conn;
   }
-  HashSet<String> viewGroups() throws SQLException {
-    PreparedStatement stmt = conn.prepareStatement("SELECT ID, TITLE FROM ITEMGROUP");
-    ResultSet rst = stmt.executeQuery();
-    System.out.println("rst= " + rst.toString());
-    HashSet<String> res = new HashSet<String>();
-    while (rst.next()) {
-      System.out.printf("row %3d:", rst.getRow());
-      int id;
-      String title;
-      id = rst.getInt(1);
-      title = rst.getString(2);
-      System.out.printf("%10d | %30s\n", id, title);
-      res.add(title);
+  Set<String> viewGroups() throws SQLException {
+    try (
+        PreparedStatement stmt = conn.prepareStatement("SELECT ID, TITLE FROM ITEMGROUP");
+        ResultSet rst = stmt.executeQuery();
+        ) {
+      System.out.println("rst= " + rst.toString());
+      HashSet<String> res = new HashSet<String>();
+      while (rst.next()) {
+        System.out.printf("row %3d:", rst.getRow());
+        int id;
+        String title;
+        id = rst.getInt(1);
+        title = rst.getString(2);
+        System.out.printf("%10d | %30s\n", id, title);
+        res.add(title);
+      }
+      stmt.close();
+      return res;
     }
-    stmt.close();
-    return res;
   }
   void viewItems() throws SQLException {
-    PreparedStatement stmt = conn.prepareStatement("SELECT ID, TITLE, GROUPID FROM ITEM");
-    ResultSet rst = stmt.executeQuery();
+    try (
+        PreparedStatement stmt = conn.prepareStatement("SELECT ID, TITLE, GROUPID FROM ITEM");
+        ResultSet rst = stmt.executeQuery()
+        ) {
     System.out.println("rst= " + rst.toString());
     rst.getRow();
     while (rst.next()) {
@@ -86,25 +94,33 @@ public class DBTester {
       groupid = rst.getInt(3);
       System.out.printf("%10d | %30s | %10d\n", id, title, groupid);
     }
-    stmt.close();
+    }
+    
   }
   int getTitleID(String table, String key) throws SQLException {
-    PreparedStatement stmt = conn.prepareStatement("SELECT ID FROM " + table + " WHERE TITLE LIKE ?");
-    stmt.setString(1, key);
-    ResultSet rst = stmt.executeQuery();
-    System.out.println("rst= " + rst.toString());
-    rst.next();
-    return rst.getInt(1);
+    try(
+      PreparedStatement stmt = conn.prepareStatement("SELECT ID FROM " + table + " WHERE TITLE LIKE ?");
+        ){
+      stmt.setString(1, key);
+      ResultSet rst = stmt.executeQuery();
+      System.out.println("rst= " + rst.toString());
+      rst.next();
+      return rst.getInt(1);
+    }
+    
   }
   void viewItemsInGroup(int groupid) throws SQLException {
-    PreparedStatement stmt = conn.prepareStatement("SELECT TITLE FROM ITEM WHERE GROUPID = ?");
-    stmt.setInt(1, groupid);
-    ResultSet rst = stmt.executeQuery();
-    System.out.println("rst= " + rst.toString());
-    while (rst.next()) {
-      System.out.printf("row %3d:", rst.getRow());
-      String title = rst.getString(1);
-      System.out.printf("%30s\n", title);
+    try(
+      PreparedStatement stmt = conn.prepareStatement("SELECT TITLE FROM ITEM WHERE GROUPID = ?");
+        ){
+      stmt.setInt(1, groupid);
+      ResultSet rst = stmt.executeQuery();
+      System.out.println("rst= " + rst.toString());
+      while (rst.next()) {
+        System.out.printf("row %3d:", rst.getRow());
+        String title = rst.getString(1);
+        System.out.printf("%30s\n", title);
+      }
     }
   }
   void viewItemsInGroup(String groupname) throws SQLException {
@@ -124,15 +140,21 @@ public class DBTester {
       String itemGroupCreator = "CREATE TABLE ITEMGROUP "
       +  "(ID INTEGER PRIMARY KEY generated always as identity, "
       +  "TITLE VARCHAR(100)  UNIQUE NOT NULL) ";
-      PreparedStatement statement = conn.prepareStatement(itemGroupCreator);
-      statement.execute();
+      try(
+        PreparedStatement statement = conn.prepareStatement(itemGroupCreator);
+          ){
+        statement.execute();
+      }
       System.out.println("table ITEMGROUP created");
       String itemGroupSetter = "INSERT INTO ITEMGROUP(TITLE) VALUES(?) ";
-      statement = conn.prepareStatement(itemGroupSetter);
-      String[] itemNames = {"КОМПЬЮТЕРЫ", "ТЕЛЕФОНЫ", "ТЕЛЕВИЗОРЫ", "ПЛЕЕРЫ"};
-      for (int i = 0; i < itemNames.length; i++) {
-        statement.setString(1, itemNames[i]);
-        statement.executeUpdate();
+      try (
+          PreparedStatement statement = conn.prepareStatement(itemGroupSetter);
+          ) {
+        String[] itemNames = {"КОМПЬЮТЕРЫ", "ТЕЛЕФОНЫ", "ТЕЛЕВИЗОРЫ", "ПЛЕЕРЫ"};
+        for (int i = 0; i < itemNames.length; i++) {
+          statement.setString(1, itemNames[i]);
+          statement.executeUpdate();
+        }
       }
     } else {
       System.out.println("table ITEMGROUP is exist");
@@ -141,27 +163,34 @@ public class DBTester {
       String itemGroupCreator = "CREATE TABLE ITEM (ID INTEGER PRIMARY KEY generated always as identity,\n"
           + "TITLE VARCHAR(100) UNIQUE NOT NULL, GROUPID INTEGER,\n"
           + "FOREIGN KEY (GROUPID) REFERENCES ITEMGROUP(ID) ) ";
-      PreparedStatement statement = conn.prepareStatement(itemGroupCreator);
-      statement.execute();
+      try(
+          PreparedStatement statement = conn.prepareStatement(itemGroupCreator);
+          ){
+          statement.execute();  
+      }
       System.out.println("table ITEM created");
       String itemSetter = "INSERT INTO ITEM(TITLE, GROUPID) VALUES(?,?) ";
-      statement = conn.prepareStatement(itemSetter);
-      int[] itemGroupIds = {1, 1, 2, 2, 3, 3};
-      String[] itemNames = {"Apple", "Dell", "HTC", "Nokia", "LG", "SAMSUNG"};
-      for (int i = 0; i < itemGroupIds.length; i++) {
-        statement.setString(1, itemNames[i]);
-        statement.setInt(2, itemGroupIds[i]);
-        statement.executeUpdate();
+      try (
+        PreparedStatement statement = conn.prepareStatement(itemSetter);
+          ){
+        int[] itemGroupIds = {1, 1, 2, 2, 3, 3};
+        String[] itemNames = {"Apple", "Dell", "HTC", "Nokia", "LG", "SAMSUNG"};
+        for (int i = 0; i < itemGroupIds.length; i++) {
+          statement.setString(1, itemNames[i]);
+          statement.setInt(2, itemGroupIds[i]);
+          statement.executeUpdate();
+        }
       }
     } else {
       System.out.println("table ITEM is exist");
     }
   }
   boolean addItemToGroup(String itemName, String groupName) {
+    PreparedStatement statement = null;
     try {
       int id = getTitleID("ITEMGROUP", groupName);
       String itemSetter = "INSERT INTO ITEM(TITLE, GROUPID) VALUES(?,?) ";
-      PreparedStatement statement = conn.prepareStatement(itemSetter);
+      statement = conn.prepareStatement(itemSetter);
       statement.setString(1, itemName);
       statement.setInt(2, id);
       statement.executeUpdate();
@@ -169,18 +198,33 @@ public class DBTester {
 //      Logger.getLogger(DBTester.class.getName()).log(Level.SEVERE, null, ex);
       return false;
     }
+    finally{
+      try {
+        statement.close();
+      } catch (SQLException ex) {
+        Logger.getLogger(DBTester.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
     return true;
   }
   boolean removeItemFromGroup(String itemName, String groupName) {
+    PreparedStatement statement = null;
     try {
       String itemSetter = "DELETE FROM ITEM WHERE TITLE=?";
-      PreparedStatement statement = conn.prepareStatement(itemSetter);
+      statement = conn.prepareStatement(itemSetter);
       statement.setString(1, itemName);
 //      statement.setInt(2, getTitleID("ITEMGROUP", groupName));
       statement.executeUpdate();
     } catch (SQLException ex) {
 //      Logger.getLogger(DBTester.class.getName()).log(Level.SEVERE, null, ex);
       return false;
+    }
+    finally{
+      try {
+        statement.close();
+      } catch (SQLException ex) {
+        Logger.getLogger(DBTester.class.getName()).log(Level.SEVERE, null, ex);
+      }
     }
     return true;
   }
@@ -204,18 +248,20 @@ public class DBTester {
         }
       }
       conn.commit();
-      conn.setAutoCommit(true);
     } catch (IOException ex) {
       Logger.getLogger(DBTester.class.getName()).log(Level.SEVERE, null, ex);
     }
+    finally{
+      conn.setAutoCommit(true);
+    }
   }
   void updateGroupsAcrossFile(String path) throws SQLException {
+    BufferedReader br;
+    HashSet<String> addSet = new HashSet<String>();
+    HashSet<String> rmSet = new HashSet<String>();
     try {
-      BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path), "utf8"));
+      br = new BufferedReader(new InputStreamReader(new FileInputStream(path), "utf8"));
       String s;
-      conn.setAutoCommit(false);
-      HashSet<String> addSet = new HashSet<String>();
-      HashSet<String> rmSet = new HashSet<String>();
       while ((s = br.readLine()) != null) {
         if (s.substring(0, 1).equals("+")) {
           addSet.add(s.substring(1));
@@ -223,7 +269,14 @@ public class DBTester {
           rmSet.add(s.substring(1));
         }
       }
-      HashSet<String> groups = viewGroups();
+    } catch (IOException ex) {
+//        Logger.getLogger(DBTester.class.getName()).log(Level.SEVERE, null, ex);
+      System.out.println("С файлом проблемы");
+      return;
+    }
+    conn.setAutoCommit(false);
+    try {
+      Set<String> groups = viewGroups();
       addSet = addSet.stream()
           .filter((String group) -> !groups.contains(group))
           .collect(Collectors.toCollection(HashSet::new));
@@ -244,11 +297,60 @@ public class DBTester {
         statementSetter.executeUpdate();
       }
       conn.commit();
-      conn.setAutoCommit(true);
-    } catch (IOException ex) {
+    } catch (SQLException ex) {
       conn.rollback();
-      conn.setAutoCommit(true);
       Logger.getLogger(DBTester.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    finally{
+      conn.setAutoCommit(true);
+    }
+  }
+  void updateGroupsAcrossFile2(String path) throws SQLException {
+    BufferedReader br;
+    HashSet<String> addSet = new HashSet<String>();
+    HashSet<String> rmSet = new HashSet<String>();
+    try {
+      br = new BufferedReader(new InputStreamReader(new FileInputStream(path), "utf8"));
+      String s;
+      while ((s = br.readLine()) != null) {
+        if (s.substring(0, 1).equals("+")) {
+          addSet.add(s.substring(1));
+        } else {
+          rmSet.add(s.substring(1));
+        }
+      }
+    } catch (IOException ex) {
+//        Logger.getLogger(DBTester.class.getName()).log(Level.SEVERE, null, ex);
+      System.out.println("С файлом проблемы");
+      return;
+    }
+    conn.setAutoCommit(false);
+    try {
+      PreparedStatement stmt = conn.prepareStatement(
+          "SELECT TITLE FROM ITEMGROUP",
+          ResultSet.TYPE_SCROLL_INSENSITIVE,
+          ResultSet.CONCUR_UPDATABLE);
+      ResultSet rs = stmt.executeQuery();
+      while (rs.next()) {
+        String group = rs.getString(1);
+        if (rmSet.contains(group)) {
+          rs.deleteRow();
+        }
+        if (addSet.contains(group)) {
+          addSet.remove(group);
+        }
+      }
+      for (String group : addSet) {
+        rs.moveToInsertRow();
+        rs.updateString("TITLE", group);
+        rs.insertRow();
+      }
+      conn.commit();
+    } catch (SQLException ex) {
+      conn.rollback();
+      Logger.getLogger(DBTester.class.getName()).log(Level.SEVERE, null, ex);
+    } finally {
+      conn.setAutoCommit(true);
     }
   }
 }
